bd9397ff805192fb49d0b6c65e3f4854a3fbc175
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-02 00:06:32 +0800

    partially completed assignment 4

--- Exercise 4 - Class JS/cars.js
+++ Exercise 4 - Class JS/cars.js
@@ -21,9 +21,30 @@ function suv(brand, model, engine, fourWheelDrive) {
   this.fourWheelDrive = fourWheelDrive;
 }
 
+suv.prototype = new Cars();
+suv.prototype.constructor = suv;
+
+var suv = new suv("Mercedes-Benz", "GLE-Class GLE400", "Petrol", "Yes");
+console.log(suv);
+
 function mpv(brand, model, engine, seater) {
   Cars.call(this, brand, model, engine);
   this.seater = seater;
 }
 
-function hatchBack(brand, model, engine,)
+mpv.prototype = new Cars();
+mpv.prototype.constructor = mpv;
+
+var mpv = new mpv("BMW", "2 Series Gran Tourer 216d", "Diesel", "7");
+console.log(mpv);
+
+function hatchBack(brand, model, engine, doors) {
+  Cars.call(this, brand, model, engine);
+  this.doors = doors;
+}
+
+hatchBack.prototype = new Cars();
+hatchBack.prototype.constructor = hatchBack;
+
+var hatchBack = new hatchBack("Volkswagen", "Golf Mk8 GTI", "Petrol", "5");
+console.log(hatchBack);
