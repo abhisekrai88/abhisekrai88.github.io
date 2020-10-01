b0130f1e7ac2f7dd79f3aa9c5fd0921dfd99375c
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-01 20:14:25 +0800

    edited to show array output together

--- Exercise 3 - Functions in JS/functions.js
+++ Exercise 3 - Functions in JS/functions.js
@@ -1,7 +1,10 @@
-const arrays1 = [1, 2];
-const arrays2 = [7, 8];
+const array1 = [1, 2];
+const array2 = [7, 8];
 
-function addingArrays() {
-  alert(arrays1[0] + arrays2[0]);
-  alert(arrays1[1] + arrays2[1]);
+function addingArrays(array1, array2) {
+  var array3 = new Array();
+  for (counter = 0; counter < array1.length; counter++) {
+    array3[counter] = array1[counter] + array2[counter];
+  }
+  console.log(array3);
 }
