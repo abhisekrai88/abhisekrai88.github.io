ea8f5aa704db786e31e7014f59bc494965d590be
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-08 20:13:07 +0800

    added comments in line 18

--- JSBasics/ES6/js/arrowfunctions.js
+++ JSBasics/ES6/js/arrowfunctions.js
@@ -10,10 +10,9 @@
 //     return a + b;
 // }
 
-
 // var sum = (a, b) => {
 //     return a + b;
 // }
 
 var sum = (a, b) => a + b;
-
+// for arrow functions, if its single line no curly braces is required. Multiple lines will require curly braces.
