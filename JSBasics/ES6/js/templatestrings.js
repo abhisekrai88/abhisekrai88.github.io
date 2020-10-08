9401b37b25192061024cbf2d746fdb21bea87252
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-08 22:11:00 +0800

    strings

--- JSBasics/ES6/js/templatestrings.js
+++ JSBasics/ES6/js/templatestrings.js
@@ -1,5 +1,21 @@
 function addTwoNumbers(a, b = 0) {
-    var sum = a + b;
-    console.log(`The sum of ${a} and ${b} is ${sum}`);
-    return 'The sum of ${a} and ${b} is ${sum}';
+  var sum = a + b;
+  console.log(`The sum of ${a} and ${b} is ${sum}`);
+  return "The sum of ${a} and ${b} is ${sum}";
 }
+
+var fullNameOld = function (firstName, lastName) {
+  var bool1 = new Boolean("Abhisek");
+  var bool2 = new Boolean(false);
+  console.log(bool1);
+  console.log(bool2);
+  return true + false;
+};
+
+const fullName = function (firstName, lastName) {
+  return `my full name is ${firstName} ${lastName}`;
+};
+
+console.log(fullNameOld("Abhisek", "Rai"));
+
+console.log(fullName("Abhisek", "Rai"));
