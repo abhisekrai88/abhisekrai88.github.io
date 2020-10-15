31e90dea474fc4180a5fccb34d39429ac167ac74
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-15 14:50:56 +0800

    changes to product cat

---
+++ Personal Portfolio/js/productCatalog.js
@@ -0,0 +1,6 @@
+let myObj = [
+    {"ID":1,"Name":"Adidas Originals Superstar (White)","Catergory":"Footwear","Price":148.99},
+    {"ID":2,"Name":"Unrvlld Leather Varsity Bomber Jacket (Red)","Catergory":"Clothing","Price":240.99},
+    {"ID":3,"Name":"Pizza Planet T-Shirt (Black)","Catergory":"Clothing","Price":27.99}
+]
+console.log(myObj)
