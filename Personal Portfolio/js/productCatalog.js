63911c5d11cfe02bc6e56ab703cfc814a9c0985e
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-17 00:05:56 +0800

    adding url link to links

--- Personal Portfolio/js/productCatalog.js
+++ Personal Portfolio/js/productCatalog.js
@@ -1,18 +1,23 @@
 let xhttp = new XMLHttpRequest();
 let txt = "";
 let price = [];
+
 xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
         let productsCat = JSON.parse(this.responseText);
 
         for (let i=0; i <productsCat.length; i++){
+            txt += '<a href ="' + productsCat[i].url +'">';
             txt +="<div class='products' id='" + productsCat[i].ID + "'>" + "Desc: " + productsCat[i].Name;
             txt += "<br>" + "Catergory: " + productsCat[i].Catergory;
             txt += "<p class='currencytext'>" + "Price: ($)" + "</p>" + "<p class = 'price'>" + productsCat[i].Price + "</p>" + "</div>";
             //txt += "<p class = 'price'>" + productsCat[i].Price + "</p>" + "</div>";
             price.push(productsCat[i].Price);
+            
         }
         document.getElementById("container").innerHTML = txt;
+        let img = document.createElement('img');
+        
     }
 
 }
