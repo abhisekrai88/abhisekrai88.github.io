5ae743cfc3782e5cde0394c5abea53c6bc045779
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-21 22:27:54 +0800

    contact me fields with textareas

---
+++ Personal Portfolio/js/contactme.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    if($('.character-counter').length > 0){
+      var maximumCharacters = 50;
+      
+      $('firstname').keyup(function(){
+        var $this = $(this);
+        var $parentElement = $this.parent();
+        var $characterCounter = $parentElement.find('.character-counter');
+        var $charactersRemaining = $parentElement.find('.characters-remaining');
+        
+        var typedText = $this.val();
+        var textLength = typedText.length;
+        var charactersRemaining = maximumCharacters - textLength; 
+        
+        // chop the text to the desired length
+        if(charactersRemaining <= 0){
+          $this.val(typedText.substr(0, maximumCharacters));
+          charactersRemaining = 0;
+          textLength = maximumCharacters;
+        }      
+        if($characterCounter.length){
+          $characterCounter.text(textLength);
+        }
+        if($charactersRemaining.length){
+          $charactersRemaining.text(charactersRemaining);
+        }
+      });
+    }
+  
+  });
+  
