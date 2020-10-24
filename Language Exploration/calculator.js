9706dc883d4df309e667bc9a04603121372ecb33
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-24 12:57:32 +0800

    summing up arrays

--- Language Exploration/calculator.js
+++ Language Exploration/calculator.js
@@ -31,3 +31,17 @@ function addNumbersFromTwoFunctions(integer1, integer2){
 function subtract(a, b){
     return a - b;
 }
+
+//to add up 2 arrays
+/*function sumArray(a1,a2) {
+    let a3 = [];
+    for (let i = 0; i < Math.max(a1.length, a2.length); i++) {
+        a3.push((a1[i] || 0) + (a2[i] || 0));
+    }
+    let result = 0;
+    for (let i = 0; i < a3.length; i++) {
+    result += a3[i];
+    }
+    return result;
+    }*/
+    
