8975cb002b702cc14e2cb00e80c1fe7117a9f0ce
abhisekrai88 <70102099+abhisekrai88@users.noreply.github.com>
2020-10-27 19:34:40 +0800

    added pictures to add into json file

--- Personal Portfolio/js/productCatalog.js
+++ Personal Portfolio/js/productCatalog.js
@@ -8,7 +8,7 @@ xhttp.onreadystatechange = function(){
 
         for (let i=0; i <productsCat.length; i++){
             txt += '<a href ="' + productsCat[i].url +'" target="_blank">';
-            txt +="<div class='products' id='" + productsCat[i].ID + "'>" + "Desc: " + productsCat[i].Name;
+            txt +="<div class='products' id='" + productsCat[i].ID + "'>" + "Desc: " + productsCat[i].Name + productsCat[i].image;
             txt += "<br>" + "Catergory: " + productsCat[i].Catergory;
             txt += "<p class='currencytext'>" + "Price: ($)" + "</p>" + "<p class = 'price'>" + productsCat[i].Price + "</p>" + "</div>";
             //txt += "<p class = 'price'>" + productsCat[i].Price + "</p>" + "</div>";
