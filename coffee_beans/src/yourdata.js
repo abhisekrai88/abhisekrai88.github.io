2013d18cb1eae62ac711792305f04a416c8b659e
Abhisek Rai <kesihba.iar@gmail.com>
2020-11-16 19:50:44 +0800

    removed unwanted comments

--- coffee_beans/src/yourdata.js
+++ coffee_beans/src/yourdata.js
@@ -17,10 +17,10 @@ export default
         aboutImage:'https://giffiles.alphacoders.com/269/2696.gif',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
-       // Change Projects Here 
+        
        projects:[
            {
-            id: 1,//DO NOT CHANGE THIS (Please)😅
+            id: 1,
             name:'Caffe Latte', //Project Title - Add Your Project Title Here
              description:'Our dark, rich espresso balanced with steamed milk and a light layer of foam. A perfect milk forward warm up.', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
@@ -29,7 +29,7 @@ export default
              url: "https://cdnimg.webstaurantstore.com/uploads/blog/2019/4/coffee-drinks_caffe-latte.jpg"
             },
             {
-                id: 2,//DO NOT CHANGE THIS (Please)😅
+                id: 2,
                 name: 'Caffe Moche',
                 description: 'We combine our rich, full-bodied espresso with bittersweet mocha sauce and steamed milk, then top it off with sweetened whipped cream. The classic coffee drink to satisfy your sweet tooth.',
                 imageSrc: "https://www.differencebetween.info/sites/default/files/images/4/mocha.jpg",
@@ -37,7 +37,7 @@ export default
                 url: "https://cdnimg.webstaurantstore.com/uploads/blog/2019/4/coffee-drinks_mocha.jpg"
             },
             { 
-                id: 3,//DO NOT CHANGE THIS (Please)😅
+                id: 3,
                 name: 'Flat White',
                 description: 'Bold ristretto shots of espresso get the perfect amount of steamed whole milk to create a not too strong, not too creamy, just right flavor.',
                 imageSrc: "https://coffeeordie.com/wp-content/uploads/2019/08/AdobeStock_162161094-scaled.jpeg",
@@ -45,7 +45,7 @@ export default
                 url: "https://cdnimg.webstaurantstore.com/uploads/blog/2019/4/coffee-drinks_flat-white.jpg"
             },
             {
-                id: 4,//DO NOT CHANGE THIS (Please)😅
+                id: 4,
                 name: 'Macchiato',
                 description: 'Meaning “stained” - Described as ‘strong, marked or stained’. A touch of steamed foamed milk added to a double shot of coffee extract made from 24gm of fresh ground beans producing 75ml of essence. Served in glass.',
                 imageSrc: "https://www.nespresso.com/ncp/resizer.php?width=400&height=300&file=uploads%2Frecipes%2Fnespresso-recipes-Espresso-Macchiato.jpg&token=1f19807fb0f3e5910dfdfc33d70ddf9e",
@@ -54,7 +54,7 @@ export default
            }
            ,
             {
-                id: 5,//DO NOT CHANGE THIS (Please)😅
+                id: 5,
                 name: 'Long Black',
                 description: 'Often called the “American”. It is the ‘benchmark coffee without milk’. It is pure coffee made from one & one half shots of coffee extract made on 16gm of fresh ground beans producing 50ml of essence blended with steamed water. Served in a pre heated vitrified ceramic cup with the essence floated over the top of a cup filled with hot/boiling water. It is a standard espresso (Short Black) but lengthened by the addition of hot/boiling water.',
                 imageSrc: "https://cdn.shopify.com/s/files/1/0258/0329/2747/products/Short_Black_20172049-8fda-437d-9107-2d13d83dbecc_300x300.jpg?v=1580089827",
@@ -63,7 +63,7 @@ export default
            }
            ,
             {
-                id: 6,//DO NOT CHANGE THIS (Please)😅
+                id: 6,
                 name: 'Cortado',
                 description: 'means cut in Spanish so the double shot espresso served in a demetesse glass supported with a metal handle is cut with an equal part of hot milk, making it in between the size and strength of a macchiato and a cappuccino. It is popular in Spain and Portugal, as well as throughout Latin America and Cuba, where it is drunk in the afternoon. Variations include more froth on top than a traditional cortado and occasionally with different names such as Piccolo or Gibraltar.',
                 imageSrc: "https://hespokestyle.com/wp-content/uploads/2014/03/hss-artifact-cortado.jpg",
@@ -73,7 +73,7 @@ export default
            }
            ,
             {
-                id: 7,//DO NOT CHANGE THIS (Please)😅
+                id: 7,
                 name: 'Cappuccino',
                 description: 'A ‘traditional morning heart starter’. Steamed foamed velvety milk poured over one shot (1) of coffee oil extract made from 12gm of freshly ground beans producing 38ml of essence. Finished by topping with foam and a sprinkle of chocolate powder. Served in a pre heated vitrified ceramic cup.',
                 imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTqQoj_m1d2I7Birf8vkkeXKJu7OZZA4nThyQ&usqp=CAU",
@@ -82,7 +82,7 @@ export default
            }
            ,
             {
-                id: 8,//DO NOT CHANGE THIS (Please)😅
+                id: 8,
                 name: 'Affogato',
                 description: 'This is a term that literally means drowned. It is the description of a shot of separately served espresso that is later poured over a the top of a scoop of vanilla ice cream or gelato. This beverage is usually served in a short drink glass and is a Italian desert favourite. Popular Affogatos include Vanilla Affogato, Mocha Affogato, and Peppermint Affogato.',
                 imageSrc: "https://media.soscuisine.com/images/recettes/large/232.jpg",
@@ -91,18 +91,6 @@ export default
 
            }
           
-
-                    /*
-
-                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
-                ,{
-                id: 5,
-                title: 'Project Five',
-                service: 'Something Amazing',
-                imageSrc: "",
-                url: ''
-            }
-                */
         ],
         social: [
             // Add Or Remove The Link Accordingly
